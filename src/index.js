076ab3a89d675615fe49b3c39558dc950a6e035a
Twister <99.twister@gmail.com>
2024-08-17 15:13:45 +0200

    add tab-switching logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import loadHomePage from "./home.js";
 import loadMenuPage from "./menu.js";
 import loadAboutPage from "./about.js";
+
+function clearPage() {
+  const content = document.getElementById("content");
+  content.innerHTML = null;
+}
+
+loadHomePage();
+
+document.getElementById("homeBtn").addEventListener("click", () => {
+  clearPage();
+  loadHomePage();
+});
+
+document.getElementById("menuBtn").addEventListener("click", () => {
+  clearPage();
+  loadMenuPage();
+});
+
+document.getElementById("aboutBtn").addEventListener("click", () => {
+  clearPage();
+  loadAboutPage();
+});
